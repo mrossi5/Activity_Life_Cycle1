d9d50d44cc0f7d443078ad08b719a9d1364c76e5
Mark <mrossi5@dtcc.edu>
2017-01-19 13:07:43 -0500

    Final complete without Comments

--- app/src/main/java/edu/dtcc/cis282student/activity_life_cycle/MainActivity.java
+++ app/src/main/java/edu/dtcc/cis282student/activity_life_cycle/MainActivity.java
@@ -3,17 +3,8 @@ package edu.dtcc.cis282student.activity_life_cycle;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Button;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate");
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
-
-
+        Snackbar
+                .make(findViewById(android.R.id.content), "OnCreate Snackbar", Snackbar.LENGTH_LONG)
+                .show();
     }
 
     @Override
@@ -34,8 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Log.i(TAG, "onStart");
         Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
-        Snackbar snackbar = Snackbar
-                .make(findViewById(android.R.id.content), "Replace with your own action", Snackbar.LENGTH_LONG);
+        Snackbar
+                .make(findViewById(android.R.id.content), "Onstart Snackbar", Snackbar.LENGTH_LONG)
+                .show();
     }
 
     @Override
@@ -43,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
         Log.i(TAG, "onRestart");
         Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+        Snackbar
+                .make(findViewById(android.R.id.content), "OnRestart Snackbar", Snackbar.LENGTH_LONG)
+                .show();
     }
 
     @Override
@@ -50,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.i(TAG, "onResume");
         Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        Snackbar
+                .make(findViewById(android.R.id.content), "OnResume Snackbar", Snackbar.LENGTH_LONG)
+                .show();
     }
 
     @Override
@@ -57,6 +56,9 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         Log.i(TAG, "onPause");
         Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        Snackbar
+                .make(findViewById(android.R.id.content), "OnPause Snackbar", Snackbar.LENGTH_LONG)
+                .show();
     }
 
     @Override
@@ -64,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         Log.i(TAG, "onStop");
         Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+        Snackbar
+                .make(findViewById(android.R.id.content), "OnStop Snackbar", Snackbar.LENGTH_LONG)
+                .show();
     }
 
     @Override
@@ -71,5 +76,8 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i(TAG, "onDestroy");
         Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        Snackbar
+                .make(findViewById(android.R.id.content), "OnDestroy Snackbar", Snackbar.LENGTH_LONG)
+                .show();
     }
 }
